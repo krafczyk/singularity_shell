e017461044419b0fce71c333c40e385b387f9036
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2023-09-01 11:43:39 -0500

    Fix the script

--- singularity_shell.sh
+++ singularity_shell.sh
@@ -2,48 +2,40 @@
 
 # Check if a given device is a block device
 isBlockDevice() {
-  local device=$1
-  [[ $(ls -l "$device" 2>/dev/null | cut -c1) == "b" ]]
+  [[ $(ls -l "$1" 2>/dev/null | cut -c1) == "b" ]]
 }
 
 # Check if a given device is a loop device
 isLoopDevice() {
-  local device=$1
-  losetup "$device" >/dev/null 2>&1
+  losetup "$1" >/dev/null 2>&1
 }
 
 # Create a mapping of device names to their corresponding mount points
 getDfOutputMap() {
-  declare -A dfMap
-  while read -r source target; do
-    [[ $source == "/dev/"* ]] && dfMap["$source"]="$target"
-  done < <(df --output=source,target | tail -n +2)
-  echo "${dfMap[@]}"
+  df --output=source,target | tail -n +2 | awk '$1 ~ /^\/dev\// {print $1, $2}'
 }
 
 # Get mount points of non-loopback block devices
 getMountPoints() {
-  local -n dfMap=$1
-  local mountPoints=()
-  for device in "${!dfMap[@]}"; do
+  mountPoints=()
+  while read -r device mount; do
     if isBlockDevice "$device" && ! isLoopDevice "$device"; then
       mountPoints+=("$device")
     fi
-  done
+  done <<< "$1"
   echo "${mountPoints[@]}"
 }
 
 # Generate volume arguments for container runtime
 generateVolumeArgs() {
-  local -n dfMap=$1
-  local -n mountPoints=$2
-  local volumeArgs=()
-  for device in "${mountPoints[@]}"; do
-    local mount=${dfMap[$device]}
-    if [ "$mount" != "/" ]; then
-      volumeArgs+=("--bind" "$mount:$mount")
+  volumeArgs=()
+  while read -r device mount; do
+    if [[ " ${mountPoints[@]} " =~ " ${device} " ]]; then
+      if [ "$mount" != "/" ]; then
+        volumeArgs+=("--bind" "$mount:$mount")
+      fi
     fi
-  done
+  done <<< "$1"
   echo "${volumeArgs[@]}"
 }
 
@@ -52,15 +44,12 @@ main() {
   # Store passed arguments
   cmdArgs="$*"
 
-  declare -A dfMap
-  read -ra dfMap <<< "$(getDfOutputMap)"
-  read -ra mountPoints <<< "$(getMountPoints dfMap)"
-  read -ra volumeArgs <<< "$(generateVolumeArgs dfMap mountPoints)"
-
-  echo ${volumeArgs[@]}
+  dfOutput=$(getDfOutputMap)
+  mountPoints=($(getMountPoints "$dfOutput"))
+  volumeArgs=($(generateVolumeArgs "$dfOutput"))
 
   # Execute singularity shell with volume and additional arguments
-  #eval "singularity shell $volumeArgs $cmdArgs"
+  eval "singularity shell ${volumeArgs[@]} $cmdArgs"
 }
 
 main "$@"
